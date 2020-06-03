7c23d3d6d566b6311d9aaa1edb89eaf714992c4f
Phil <pchapy@gmail.com>
2020-06-03 16:24:28 -0700

    Maps API Initialized, default set to DC Park

---
+++ parkhangs-webapp/src/components/Map.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+import GoogleMapReact from 'google-map-react';
+
+const AnyReactComponent = ({ text }) => <div>{text}</div>;
+
+class SimpleMap extends Component {
+  static defaultProps = {
+    center: {
+      lat: 49.28,
+      lng: -123.12
+    },
+    zoom: 11
+  };
+
+  render() {
+    return (
+      // Important! Always set the container height explicitly
+      <div style={{ height: '400px', width: '100%' }}>
+        <GoogleMapReact
+          bootstrapURLKeys={{ key: 'AIzaSyBjhoRipAbC34T3BhY9q6l2Ktu4_HjX9Q4' }}
+          defaultCenter={this.props.center}
+          defaultZoom={this.props.zoom}
+        >
+          <AnyReactComponent
+            lat={49.264012}
+            lng={-123.095931}
+            text="Dude Chilling Park"
+          />
+        </GoogleMapReact>
+      </div>
+    );
+  }
+}
+
+export default SimpleMap;
