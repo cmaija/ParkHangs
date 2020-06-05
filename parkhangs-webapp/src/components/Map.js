21ba1c96b18d49a01e9668fe1338770e49ba9e06
Phil <pchapy@gmail.com>
2020-06-05 14:22:06 -0700

    added a couple parks

--- parkhangs-webapp/src/components/Map.js
+++ parkhangs-webapp/src/components/Map.js
@@ -9,15 +9,15 @@ class SimpleMap extends Component {
       lat: 49.28,
       lng: -123.12
     },
-    zoom: 11
+    zoom: 12
   };
 
   render() {
     return (
       // Important! Always set the container height explicitly
-      <div style={{ height: '400px', width: '100%' }}>
+      <div style={{ height: '500px', width: '75%', margin: 'auto' }}>
         <GoogleMapReact
-          bootstrapURLKeys={{ key: 'API_key' }}
+          bootstrapURLKeys={{ key: 'API Key' }}
           defaultCenter={this.props.center}
           defaultZoom={this.props.zoom}
         >
@@ -26,6 +26,19 @@ class SimpleMap extends Component {
             lng={-123.095931}
             text="Dude Chilling Park"
           />
+
+          <AnyReactComponent
+            lat={49.2557}
+            lng={-123.1351}
+            text="Shaughnessy Park"
+          />
+
+          <AnyReactComponent
+            lat={49.2800}
+            lng={-123.1387}
+            text="Sunset Beach Park"
+          />
+
         </GoogleMapReact>
       </div>
     );
