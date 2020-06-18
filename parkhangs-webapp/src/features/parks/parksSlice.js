781a239bb39d9e6e0c5e4e6177bdff5fe13a9c72
personal_user <lydiamzheng@gmail.com>
2020-06-18 11:50:58 -0700

    Added simple event mock data to parkslice; UTC offset for vancouver is UTC-7

--- parkhangs-webapp/src/features/parks/parksSlice.js
+++ parkhangs-webapp/src/features/parks/parksSlice.js
@@ -4,7 +4,12 @@ import { createSlice } from '@reduxjs/toolkit'
 const parksSlice = createSlice({
     name: 'parks',
 
-    initialState: [],
+    initialState: [
+        ["Dude Chilling Park", "2020-07-16T19:20-07:00"],
+        ["Shaughnessy Park", "2020-08-01T13:30-07:00"],
+        ["Sunset Beach Park", "2020-06-29T10:45-07:00"]
+
+    ],
 
     reducers: {
         addPark: {
