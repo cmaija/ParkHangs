85c6ae67630de12f46a452c6ceca34211cc36fd7
lydia-zheng <36296057+lydia-zheng@users.noreply.github.com>
2020-06-18 15:56:09 -0700

    Mock data (#12)
    * Added simple event mock data to parkslice; UTC offset for vancouver is UTC-7
    
    * npm installed to have updated pacakge-lock.json hope nothing's messed up
    
    * changed schema to objects
    
    * Added array of events and object wrapper to each park for key access
    
    * Added lat long to mock data

--- parkhangs-webapp/src/features/parks/parksSlice.js
+++ parkhangs-webapp/src/features/parks/parksSlice.js
@@ -4,7 +4,35 @@ import { createSlice } from '@reduxjs/toolkit'
 const parksSlice = createSlice({
     name: 'parks',
 
-    initialState: [],
+    initialState: [
+        {
+            parkName: "Dude Chilling Park",
+            lat: 49.264012,
+            lng: -123.095931,
+            events: [
+                {parkName: "Dude Chilling Park", eventTime: "2020-07-16T19:20-07:00"},
+                {parkName: "Dude Chilling Park", eventTime: "2021-01-01T11:17-07:00"}
+            ]
+        },
+        {
+            parkName: "Shaughnessy Park",   
+            lat: 49.2557,
+            lng: -123.1351,
+            events: [
+                {parkName: "Shaughnessy Park", eventTime: "2020-08-01T13:30-07:00"}
+            ]
+        }
+        {
+            parkName: "Sunset Beach Park",
+            lat: 49.2800,
+            lng: -123.1387,
+            events: [
+                {parkName: "Sunset Beach Park", eventTime: "2020-06-29T10:45-07:00"},
+                {parkName: "Sunset Beach Park", eventTime: "2020-07-16T19:20-07:00"},
+                {parkName: "Sunset Beach Park", eventTime: "2020-12-24T07:50-07:00"}
+            ]
+        }
+    ],
 
     reducers: {
         addPark: {
