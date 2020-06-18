b699f54beab42d43854f487843b37b9e4923afd2
personal_user <lydiamzheng@gmail.com>
2020-06-18 12:48:33 -0700

    Added array of events and object wrapper to each park for key access

--- parkhangs-webapp/src/features/parks/parksSlice.js
+++ parkhangs-webapp/src/features/parks/parksSlice.js
@@ -5,10 +5,27 @@ const parksSlice = createSlice({
     name: 'parks',
 
     initialState: [
-        {parkName: "Dude Chilling Park", eventTime: "2020-07-16T19:20-07:00"},
-        {parkName: "Shaughnessy Park", eventTime: "2020-08-01T13:30-07:00"},
-        {parkName: "Sunset Beach Park", eventTime: "2020-06-29T10:45-07:00"}
-
+        {
+            parkName: "Dude Chilling Park",
+            events: [
+                {parkName: "Dude Chilling Park", eventTime: "2020-07-16T19:20-07:00"},
+                {parkName: "Dude Chilling Park", eventTime: "2021-01-01T11:17-07:00"}
+            ]
+        },
+        {
+            parkName: "Shaughnessy Park",   
+            events: [
+                {parkName: "Shaughnessy Park", eventTime: "2020-08-01T13:30-07:00"}
+            ]
+        }
+        {
+            parkName: "Sunset Beach Park",
+            events: [
+                {parkName: "Sunset Beach Park", eventTime: "2020-06-29T10:45-07:00"},
+                {parkName: "Sunset Beach Park", eventTime: "2020-07-16T19:20-07:00"},
+                {parkName: "Sunset Beach Park", eventTime: "2020-12-24T07:50-07:00"}
+            ]
+        }
     ],
 
     reducers: {
