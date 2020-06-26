e574e73a731c0d12febf2bf4d0b8db64b98cace5
personal_user <lydiamzheng@gmail.com>
2020-06-25 20:52:15 -0700

    Got endpoint working with dummy event in mongodb; haven't tested for multiple events

--- parkhangs-expressapp/routes/index.js
+++ parkhangs-expressapp/routes/index.js
@@ -1,9 +1,13 @@
 var express = require('express');
 const controller = require('../controllers/index');
+const eventbyparkIdController = require('../controllers/getEventsByParkId');
 
 var router = express.Router();
 
 //PLACE ROUTES HERE!
 router.get('/parks', controller.getParks);
+
+
+router.get('/:parkId/events', eventbyparkIdController.getEventsByParkId);
 
 module.exports = router;
