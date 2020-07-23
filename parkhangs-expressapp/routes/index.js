5e6170a139b0c3aa17ee3efeccbcb6bc3f1e3f11
Philippe Chapdelaine <37817723+philchapdelaine@users.noreply.github.com>
2020-07-23 12:12:35 -0700

    added 6 endpoints for event/park comments (#47)

--- parkhangs-expressapp/routes/index.js
+++ parkhangs-expressapp/routes/index.js
@@ -1,6 +1,8 @@
 var express = require('express');
 const controller = require('../controllers/index');
 const eventController = require('../controllers/eventController');
+const parkCommentController = require('../controllers/parkCommentController');
+const eventCommentController = require('../controllers/eventCommentController');
 var assert = require('assert');
 var mongoose = require("mongoose");
 const eventbyparkIdController = require('../controllers/getEventsByParkId');
@@ -13,6 +15,12 @@ router.get('/parks', controller.getParks);
 router.patch('/events/:eventId', controller.updateEvent)
 router.delete('/events/:eventId', controller.deleteEvent)
 router.get('/events', eventController.getEvents);
-router.post('/events',eventController.addEvent);
+router.post('/events', eventController.addEvent);
+router.get('/parkComments', parkCommentController.getParkComments);
+router.post('/parkComments', parkCommentController.addParkComment);
+router.delete('/parkComments', parkCommentController.deleteParkComment);
+router.get('/eventComments', eventCommentController.getEventComments);
+router.post('/eventComments', eventCommentController.addEventComment);
+router.delete('/eventComments', eventCommentController.deleteEventComment);
 
 module.exports = router;
