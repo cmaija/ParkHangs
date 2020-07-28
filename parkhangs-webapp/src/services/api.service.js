2c819f3b555bd6d60932e177eb76c37815a9aa30
Phil <pchapy@gmail.com>
2020-07-28 14:41:18 -0700

    url changed back

--- parkhangs-webapp/src/services/api.service.js
+++ parkhangs-webapp/src/services/api.service.js
@@ -2,9 +2,8 @@ import axios from 'axios'
 
 const ApiService = {
     async init() {
-        axios.defaults.baseURL = "http://localhost:9000"
+        axios.defaults.baseURL =  process.env.REACT_APP_API_ROOT
     },
-    // process.env.REACT_APP_API_ROOT
 
     get(resource) {
         return axios.get(resource)
